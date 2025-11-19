3b13da1585ae8a27773b793a58f7c808dd4346a1
JayJay889 <jespermorgenstern@gmail.com>
2025-11-19 13:23:09 +0100

    FIX

--- app.py
+++ app.py
@@ -136,4 +136,4 @@ def show_person(contact_id):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True, port=port)
+    app.run(debug=True, host='0.0.0.0', port=port)
